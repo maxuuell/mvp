1caeb6ba7a41f95b2eff026693443793fa873399
maxuuell <maxuuell@gmail.com>
2017-02-07 20:24:07 -0800

    Added react className to searchbar elements.

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -5,8 +5,8 @@ export var SearchBar = ({handleSubmit, handleChange, value}) => {
     <form className="col s12" onSubmit={(e) => handleSubmit(e)}>
       <div className="row">
         <div className="input-field col s12">
-          <input type="text" class="col s12 9m" placeholder="example: Star Wars" value={value} onChange={(e) => handleChange(e.target.value)} />
-          <div class="center-align">
+          <input type="text" className="col s12 9m" placeholder="example: Star Wars" value={value} onChange={(e) => handleChange(e.target.value)} />
+          <div className="center-align">
             <button className="btn waves-effect waves-light center-align" type="submit" name="action">Submit
               <i className="material-icons right">send</i>
             </button>
